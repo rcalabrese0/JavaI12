ebc9577664714756f5f81238b9a22f68d539752a
rodrigocala <rodrigocala11@gmail.com>
2023-08-27 00:59:24 -0300

    inicializo el proyecto

---
+++ src/main/java/DesarrolloI12/prueba/PruebaApplication.java
@@ -0,0 +1,13 @@
+package DesarrolloI12.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaApplication.class, args);
+	}
+
+}
